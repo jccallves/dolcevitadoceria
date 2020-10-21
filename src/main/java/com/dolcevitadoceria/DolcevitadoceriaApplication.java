53b2f83e4637fb2733e8cce1c97df1ed43103a2b
Julio Alves <jccallves@gmail.com>
2020-10-21 08:40:03 -0300

    Projeto criado

---
+++ src/main/java/com/dolcevitadoceria/DolcevitadoceriaApplication.java
@@ -0,0 +1,13 @@
+package com.dolcevitadoceria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolcevitadoceriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolcevitadoceriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dolcevitadoceria/DolcevitadoceriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolcevitadoceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolcevitadoceriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
