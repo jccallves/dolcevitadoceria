190b7784ef68d10b33320c515020e7b0ea6befd8
Julio Alves <jccallves@gmail.com>
2020-10-21 11:20:48 -0300

    Testando a primeira classe de domínio

---
+++ src/main/java/com/dolcevitadoceria/controller/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.dolcevitadoceria.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dolcevitadoceria.domain.Categoria;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+}
