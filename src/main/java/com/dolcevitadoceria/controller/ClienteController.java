759a2b356915aa88c40d477ddeeb1154df5b8ac0
Julio Alves <jccallves@gmail.com>
2020-10-24 16:47:04 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/dolcevitadoceria/controller/ClienteController.java
+++ src/main/java/com/dolcevitadoceria/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.dolcevitadoceria.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dolcevitadoceria.domain.Cliente;
 import com.dolcevitadoceria.dto.ClienteDTO;
+import com.dolcevitadoceria.dto.ClienteNewDTO;
 import com.dolcevitadoceria.services.ClienteService;
 
 @RestController
@@ -64,4 +67,15 @@ public class ClienteController {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));  
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = cliService.fromDTO(objDto);
+		obj = cliService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+
 }
--- src/main/java/com/dolcevitadoceria/services/ClienteService.java
+++ src/main/java/com/dolcevitadoceria/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dolcevitadoceria.domain.Cidade;
 import com.dolcevitadoceria.domain.Cliente;
+import com.dolcevitadoceria.domain.Endereco;
+import com.dolcevitadoceria.domain.enums.TipoCliente;
 import com.dolcevitadoceria.dto.ClienteDTO;
+import com.dolcevitadoceria.dto.ClienteNewDTO;
 import com.dolcevitadoceria.repositories.ClienteRepository;
+import com.dolcevitadoceria.repositories.EnderecoRepository;
 import com.dolcevitadoceria.services.exceptions.DataIntegrityException;
 import com.dolcevitadoceria.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente getClienteById (Integer id) {
 		Optional<Cliente> obj = clienteRepository.findById(id);
@@ -60,5 +69,28 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = clienteRepository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 }
