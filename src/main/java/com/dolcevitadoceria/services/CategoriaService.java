45b3acd453b22cc159e462309eaa832db767e20f
Julio Alves <jccallves@gmail.com>
2020-10-24 14:29:09 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/dolcevitadoceria/services/CategoriaService.java
+++ src/main/java/com/dolcevitadoceria/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.dolcevitadoceria.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.dolcevitadoceria.domain.Categoria;
 import com.dolcevitadoceria.repositories.CategoriaRepository;
+import com.dolcevitadoceria.services.exceptions.DataIntegrityException;
 import com.dolcevitadoceria.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		getCategoriaById(obj.getId());
 		return categoriaRepository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		getCategoriaById(id);
+		try {
+			categoriaRepository.deleteById(id);;
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
