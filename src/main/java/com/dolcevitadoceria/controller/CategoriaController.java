db884490d1a6fc6eaf5364e256d0ae2b314aeddb
Julio Alves <jccallves@gmail.com>
2020-10-24 13:51:13 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/dolcevitadoceria/controller/CategoriaController.java
+++ src/main/java/com/dolcevitadoceria/controller/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.dolcevitadoceria.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dolcevitadoceria.domain.Categoria;
 import com.dolcevitadoceria.services.CategoriaService;
@@ -22,5 +26,13 @@ public class CategoriaController {
 		
 		Categoria cat = catService.getCategoriaById(id);
 		return ResponseEntity.ok().body(cat);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = catService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
