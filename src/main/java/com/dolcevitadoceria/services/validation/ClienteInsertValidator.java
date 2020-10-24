18002a6ca03001e9e497d14ac99f19dca733f55e
Julio Alves <jccallves@gmail.com>
2020-10-24 17:21:06 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/dolcevitadoceria/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/dolcevitadoceria/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.dolcevitadoceria.controller.exceptions.FieldMessage;
+import com.dolcevitadoceria.domain.Cliente;
 import com.dolcevitadoceria.domain.enums.TipoCliente;
 import com.dolcevitadoceria.dto.ClienteNewDTO;
+import com.dolcevitadoceria.repositories.ClienteRepository;
 import com.dolcevitadoceria.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
