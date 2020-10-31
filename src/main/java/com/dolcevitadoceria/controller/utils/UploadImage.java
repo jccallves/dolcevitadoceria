627b0873dbf0177d14dfdd6f12058a1e6f0a57a5
Julio Alves <jccallves@gmail.com>
2020-10-30 23:17:54 -0300

    Usando padrão de nomes para imagens salvos na pasta do projeto.

--- src/main/java/com/dolcevitadoceria/controller/utils/UploadImage.java
+++ src/main/java/com/dolcevitadoceria/controller/utils/UploadImage.java
@@ -5,10 +5,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.dolcevitadoceria.security.UserSS;
+import com.dolcevitadoceria.services.UserService;
+import com.dolcevitadoceria.services.exceptions.AuthorizationException;
 
 @Component
 public class UploadImage {
@@ -18,6 +23,9 @@ public class UploadImage {
 	
 	@Value("${client.image.folder}")
 	private String clienteFolder;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	
 	public void salvarFoto(MultipartFile foto) {
@@ -41,11 +49,18 @@ public class UploadImage {
 	public Path salvar(String clientFolder, MultipartFile arquivo) {
 		//String caminho = getClass().getResource("").getPath().substring(0,59) + "/src/main/resources/clientes";
 		
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado	!");
+		}
 
 		Path diretorioPath = Paths.get(this.path , clienteFolder);
+		String extensaoDoArquivo = "." + FilenameUtils.getExtension(arquivo.getOriginalFilename());
+		String newFileName = this.prefix + user.getId() + extensaoDoArquivo;
+		Path arquivoPath = diretorioPath.resolve(newFileName);
 		
-		Path arquivoPath = diretorioPath.resolve(arquivo.getOriginalFilename());
-		System.out.println("Caminho completo: " + arquivoPath);
+		System.out.println("Novo arquivo com prefixo: " + arquivoPath);
 		try {
 			Files.createDirectories(diretorioPath);
 			arquivo.transferTo(arquivoPath.toFile());
